d4ed344b38d33aa9a74ada0af418e67d47cd622c
robafett <ram_95@msn.com>
2015-08-15 16:47:54 +1000

    Added health, damage and 'Winning'

---
+++ GameStudioTwo/Assets/Scripts/playerHealth.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class playerHealth : MonoBehaviour {
+
+    public Slider health;
+    public Text finish;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (health.value == 0)
+        {
+            if (gameObject.CompareTag("Player"))
+            {
+                finish.text = "You Lose";
+            }
+            else if (gameObject.CompareTag("Enemy"))
+            {
+                finish.text = "You win";
+            }
+        }
+        
+
+	
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("Player"))
+        {
+            float damage = Random.Range(collision.relativeVelocity.magnitude - 2, collision.relativeVelocity.magnitude + 2);
+            Debug.Log(gameObject.tag + " hit for " + damage);
+            health.value -= damage;
+        }
+        
+    }
+}
