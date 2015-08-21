6f27ccb1de1da6b53a7252959304d90c0d61f150
robafett <ram_95@msn.com>
2015-08-22 07:36:13 +1000

    Implemented a 'boost' using spacebar as well as 360 controls

--- GameStudioTwo/Assets/Scripts/Physics/VehicleController.cs
+++ GameStudioTwo/Assets/Scripts/Physics/VehicleController.cs
@@ -9,6 +9,7 @@ public class VehicleController : MonoBehaviour
     /// </summary>
     public WheelCollider[] wheels;
 	public bool play = true;
+    public HPBar energyBar;
 
     /// <summary>
     /// Motor, braking and steering constraints
@@ -16,21 +17,58 @@ public class VehicleController : MonoBehaviour
     public float
         maxTorque = 50.0f,
         maxBrakeTorque = 50.0f,
+        maxEnergy = 50.0f,
+        speedMultiplier = 1.0f,
         steeringAngle = 20.0f;
 
     /// <summary>
     /// Controller inputs obtained at frame update
     /// </summary>
+
+    private bool
+        boosting;
+
     private float
         inputLinearForce,
         inputSteering;
+
+    private float _energy;
+
+    public float energy
+    {
+        get { return _energy; }
+        set
+        {
+            _energy = value < 0 ? 0 : value;
+            energyBar.value = _energy;
+        }
+    }
+
+    void Start()
+    {
+        energyBar.maxValue = maxEnergy;
+        energy = maxEnergy;
+    }
 
     void Update()
     {
 		if (play) 
 		{
 			// Update input forces
-			inputLinearForce = Input.GetAxis ("Vertical");
+            if (Input.GetKey(KeyCode.W) || Input.GetButton("Fire1"))
+                inputLinearForce = 1;
+            else if (Input.GetKey(KeyCode.S) || Input.GetButton("Fire2"))
+                inputLinearForce = -1;
+            else
+                inputLinearForce = 0;
+
+            if (Input.GetButton("Boost") && energy >= 1)
+            {
+                boosting = true;
+            }
+            boost();
+            
+			    //inputLinearForce = Input.GetAxis ("Vertical");
 			inputSteering = Input.GetAxis ("Horizontal");
 		}
     }
@@ -43,9 +81,28 @@ public class VehicleController : MonoBehaviour
 			wheels [0].steerAngle = wheels [1].steerAngle = steerAngle;
 
 			// All-wheel drive
-			float torque = inputLinearForce * maxTorque;
+			float torque = inputLinearForce * maxTorque * speedMultiplier;
 			for (int i = 0; i < wheels.Length; i++)
 				wheels [i].motorTorque = torque;
 		}
+    }
+
+    void boost()
+    {
+        if (energy > 0 && boosting)
+        {
+            energy -= 1;
+            speedMultiplier = 5;
+        }
+        if (energy == 0)
+        {
+            speedMultiplier = 1;
+            boosting = false;
+        }
+        if (boosting == false && energy < maxEnergy)
+        {
+            energy += 0.25f;
+        }
+
     }
 }
