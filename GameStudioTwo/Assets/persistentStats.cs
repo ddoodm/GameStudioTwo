3372b5c338376972287c6fa950a3718e21cd62a4
JesseWalker11729631 <11729631@student.uts.edu.au>
2015-09-06 02:12:01 +1000

    Sockets Added(No Functionality), Various Store UI Tweaks and Functionality Updates

--- GameStudioTwo/Assets/persistentStats.cs
+++ GameStudioTwo/Assets/persistentStats.cs
@@ -3,12 +3,13 @@ using System.Collections;
 
 public class persistentStats : MonoBehaviour {
 
-    public Color color;
+	public Color playerColor;
+	public Equipment[] playerItems;
     public bool spike;
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
