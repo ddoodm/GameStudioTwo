68a76a4c1e46f0cf285d6161fb4a5900c0c3528c
dinhvo7 <dinhvo7@gmail.com>
2015-09-06 23:24:14 +1000

    radar function

---
+++ GameStudioTwo/Assets/Scripts/MiniMap/Radar.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Radar : MonoBehaviour {
+	// Use this for initialization
+	public GameObject radarObject;
+	public GameObject borderOjbect;
+	public float switchDistance;
+	public Transform helpTransform;
+
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance (radarObject.transform.position, transform.position) > switchDistance) {
+			//switch to borderObjects
+			helpTransform.LookAt (radarObject.transform);
+			borderOjbect.transform.position = transform.position + switchDistance * helpTransform.forward;
+			borderOjbect.layer = LayerMask.NameToLayer ("Radar");
+			radarObject.layer = LayerMask.NameToLayer ("Invisible");
+		} else {
+			// switch back to radarObject
+			borderOjbect.layer = LayerMask.NameToLayer("Invisible");
+			radarObject.layer = LayerMask.NameToLayer("Radar");
+		}
+	}
+}
