855e93c782c47e81ee7d6329d7c1b3d8792a4d48
Deinyon Davies <deinyond@gmail.com>
2015-09-16 21:40:08 +1000

    BattleScene03 Beta Update

---
+++ GameStudioTwo/Assets/CameraLookatSimple.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraLookatSimple : MonoBehaviour
+{
+    public Transform target;
+    public float
+        targetFOV = 40.0f,
+        maxDist = 10.0f;
+
+    private Camera camera;
+
+	// Use this for initialization
+	void Start ()
+    {
+        camera = this.GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        this.transform.rotation = Quaternion.LookRotation(target.position - this.transform.position);
+
+        float distanceUnit = 1.0f - (target.position - this.transform.position).magnitude / maxDist;
+        camera.fieldOfView = distanceUnit * targetFOV;
+	}
+}
