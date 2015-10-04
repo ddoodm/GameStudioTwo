e8ffe0c7e59debbc0c66bc6cb55d45b0b12606a7
JesseWalker11729631 <11729631@student.uts.edu.au>
2015-10-04 18:31:36 +1100

    Added all models into scene and fixed highlighter code

--- GameStudioTwo/Assets/Scripts/UI/itemSelector.cs
+++ GameStudioTwo/Assets/Scripts/UI/itemSelector.cs
@@ -1,92 +1,100 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class itemSelector : MonoBehaviour {
+public class itemSelector : MonoBehaviour
+{
 
-	private Color startColour;
-	private Color highlightItemColour = new Color(50f/255f, 75f/255f, 255f/255f);
-	private Color highlightSocketColour = new Color(0f/255f, 200f/255f, 0f/255f);
-	private string currentSelection;
-	public bool isSocket;
-	private bool highlighted = false;
+    private Color startColour;
+    private Color highlightItemColour = new Color(50f / 255f, 75f / 255f, 255f / 255f);
+    private Color highlightSocketColour = new Color(0f / 255f, 200f / 255f, 0f / 255f);
+    private string currentSelection;
+    public bool isSocket;
+    private bool highlighted = false;
 
-	private StoreController storeController;
+    private StoreController storeController;
 
-	void Start()
-	{
-		storeController = GameObject.FindGameObjectWithTag("StoreUI").GetComponent<StoreController>();
-		startColour = GetComponentInChildren<Renderer>().material.color;
-	}
-
-	void Update (){
-
-
-		if (!highlighted) {
-			if (GetComponent<Transform>().tag != storeController.selectedEquipment.ToString()|| GetComponent<Transform> ().tag == "Phone_Model") {
-				if (!isSocket) {
-					foreach (Transform child in transform) {
-						child.GetComponent<Renderer> ().material.color = startColour;
-					}
-				} else {
-					foreach (Transform child in transform) {
-						if (child.GetComponent<Renderer> () != null){
-							child.GetComponent<Renderer> ().material.color = startColour;
-							child.GetComponent<Transform> ().localScale = new Vector3 (0.25f, 0.25f, 0.25f);
-						}
-					}
-				}
-			}
-		}
-	}
+    
+    void Start()
+    {
+        storeController = GameObject.FindGameObjectWithTag("StoreUI").GetComponent<StoreController>();
+        startColour = GetComponentInChildren<Renderer>().material.color;
+    }
 
 
-	
-	void OnMouseEnter(){
-		if (storeController.current_state != StoreState.STATE_ITEM)
-			return;
+    void Update()
+    {
+        currentSelection = storeController.selectedEquipment.ToString();
+
+        if (highlighted)
+        {
+            return;
+        }
 
 
-		highlighted = true;
-		if (GetComponent<Transform> ().tag != storeController.selectedEquipment.ToString() || GetComponent<Transform> ().tag == "Phone_Model" ) {
-			if (!isSocket) {
-				foreach (Transform child in transform) {
-					child.GetComponent<Renderer> ().material.color = highlightItemColour;
-				}
-			} else {
-				foreach (Transform child in transform) {
-					if (child.GetComponent<Renderer> () != null){
-						child.GetComponent<Renderer> ().material.color = highlightSocketColour;
-						child.GetComponent<Transform> ().localScale = new Vector3 (0.375f, 0.375f, 0.375f);
-					}
-				}
-			}
-		} else {
-			foreach (Transform child in transform) {
-				child.GetComponent<Renderer> ().material.color = storeController.selectedItemColour;
-			}
-		}
-	}
-	
-	
-	void OnMouseExit(){
-		highlighted = false;
-		if (GetComponent<Transform> ().tag != storeController.selectedEquipment.ToString()|| GetComponent<Transform> ().tag == "Phone_Model") {
-			if (!isSocket) {
-				foreach (Transform child in transform) {
-					child.GetComponent<Renderer> ().material.color = startColour;
-				}
-			} else {
-				foreach (Transform child in transform) {
-					if (child.GetComponent<Renderer> () != null){
-						child.GetComponent<Renderer> ().material.color = startColour;
-						child.GetComponent<Transform> ().localScale = new Vector3 (0.25f, 0.25f, 0.25f);
-					}
-				}
-			}
-		} else {
-			foreach (Transform child in transform) {
-				child.GetComponent<Renderer> ().material.color = storeController.selectedItemColour;
-			}
-		}
-	}
+        if (GetComponent<Transform>().tag != currentSelection)
+        {
+            foreach (Transform child in transform)
+            {
+                ResetColour(child);
+            }
+        }
+    }
+    
+
+    void OnMouseEnter()
+    {
+        if (storeController.current_state != StoreState.STATE_ITEM)
+        {
+            return;
+        }
+
+        highlighted = true;
+
+        if (GetComponent<Transform>().tag != currentSelection)
+        {
+            foreach (Transform child in transform)
+            {
+                    ChangeColour(child);
+            }
+        }
+       
+    }
+
+
+    void OnMouseExit()
+    {
+        highlighted = false;
+
+        if (GetComponent<Transform>().tag != currentSelection)
+        {
+            foreach (Transform child in transform)
+            {
+                ResetColour(child);
+            }
+        }
+        
+    }
+
+
+    public void ChangeColour(Transform transform)
+    {
+        transform.GetComponent<Renderer>().material.color = highlightItemColour;
+
+        if (isSocket)
+        {
+            transform.GetComponent<Renderer>().material.color = highlightSocketColour;
+            transform.GetComponent<Transform>().localScale = new Vector3(0.375f, 0.375f, 0.375f);
+        }
+    }
+
+
+    public void ResetColour(Transform transform)
+    {
+        transform.GetComponent<Renderer>().material.color = startColour;
+
+        if (isSocket)
+        {
+            transform.GetComponent<Transform>().localScale = new Vector3(0.25f, 0.25f, 0.25f);
+        }
+    }
 }
