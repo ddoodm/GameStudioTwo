573d108f287940b1d41013c96b1bec971335dec3
JesseWalker11729631 <11729631@student.uts.edu.au>
2015-10-11 04:58:43 +1100

    Made a few changes to the phone and other things I can't remember anymore

---
+++ GameStudioTwo/Assets/Scripts/UI/phoneScreenUI.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class phoneScreenUI : MonoBehaviour {
+
+    public Button button;
+    public int price;
+    public Equipment thisItem;
+    StoreController storeController;
+
+	void Start () {
+        storeController = GameObject.FindGameObjectWithTag("StoreUI").GetComponent<StoreController>();
+
+        if (storeController.AvailableItems[(int)thisItem] != Equipment.EMPTY)
+        {
+           // button.interactable = false;
+        }
+    }
+	
+
+    public void DisableBuy()
+    {
+        if (storeController.DOLLADOLLABILLSYALL > price)
+        {
+            button.interactable = false;
+            storeController.DOLLADOLLABILLSYALL -= price;
+            storeController.AvailableItems[(int)thisItem] = thisItem;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+}
