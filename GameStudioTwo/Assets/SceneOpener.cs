a3926db279d4c008f19093c30863f0440ecfb701
Deinyon Davies <deinyond@gmail.com>
2015-11-02 20:09:23 +1100

    Final Presentation Build

---
+++ GameStudioTwo/Assets/SceneOpener.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneOpener : MonoBehaviour {
+
+    public int sceneID = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+    public void ChangeLevel()
+    {
+        Application.LoadLevel(sceneID);
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
